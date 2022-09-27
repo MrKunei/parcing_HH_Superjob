b474972a4d94724e6c6f07b3edd4f1229e01341c
Vladimir <vladimir.kunei@gmail.com>
2022-09-27 19:00:15 +0300

    init class Vacancy

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 import requests
+import re
 
 class Engine(ABC):
     @abstractmethod
@@ -8,7 +9,7 @@ class Engine(ABC):
 
 
 class HH(Engine):
-    def __init__(self, text, num_page=8):
+    def __init__(self, text, num_page=16):
         self._text = text
         self._num_page = num_page
 
@@ -16,16 +17,20 @@ class HH(Engine):
     def text(self):
         return self._text
 
+    def num_page(self):
+        return self._num_page
+
     def get_request(self):
         vacancy_hh = []
         for num in range(self._num_page):
-            par = {"text": self.text, 'per_page': '100', 'page': self._num_page}
+            par = {"text": self.text, 'per_page': '50', 'page': self._num_page}
             response = requests.get(f"https://api.hh.ru/vacancies", params=par)
-            for i in response.json()["items"]:
+
+            for i in response.json()['items']:
                 vacancy_hh.extend([{"title": i['name'],
-                                   "salary": i['salary'],
-                                   "link": i['alternate_url'],
-                                   "description": i['snippet']['responsibility']}])
+                                       "salary": i['salary'],
+                                       "link": i['alternate_url'],
+                                       "description": i['snippet']['responsibility']}])
         return vacancy_hh
 
 
@@ -37,15 +42,34 @@ class Superjob(Engine):
 
 class Vacancy():
 
-    def __init__(self, name, urls, salary, description):
-        self.name = name
-        self.urls = urls
-        self.salary = salary
-        self.description = description
+    def __init__(self, title, urls, salary, description):
+        self._title = title
+        self._urls = urls
+        self._salary = salary
+        self._description = description
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def urls(self):
+        return self._urls
+    @property
+    def salary(self):
+        if self._salary:
+            return f" {self._salary['from']} - {self._salary['to']}"
+        else:
+            return "не указано"
+    @property
+    def descriptoin(self):
+        res = re.compile("<highlighttext>|<\/highlighttext>")
+        description = re.sub(res, "", self._description)
+        return description
 
     def __repr__(self):
-        return f"Вакансия: {self.name}" \
-               f"Описание: {self.description}" \
-               f"Уровень дохода: {self.salary}" \
-               f"Ссылка: {self.urls}"
+        return f"Вакансия: {self.title}.\n" \
+               f"Уровень дохода: от{self.salary}.\n" \
+               f"Ссылка: {self.urls}.\n" \
+               f"Описание: {self.descriptoin}.\n"
 
