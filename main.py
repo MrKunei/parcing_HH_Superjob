7c75b5cc043f6f76d91a3f630030c868a663ea0e
Vladimir <vladimir.kunei@gmail.com>
2022-09-28 23:24:32 +0300

    main 1.1

--- main.py
+++ main.py
@@ -11,27 +11,28 @@ def main():
     parcing_page(job, 17, HH)
     print("Начинаем парсить сайт Superjob")
     parcing_page(job, 3, Superjob)
-    print(f"Сбор данных закончен!\nВсего было найдено {len(read_json_file(job))} вакансий.\n")
+    data = read_json_file(job)
+    print(f"Сбор данных закончен!\nВсего было найдено {len(data)} вакансий.\n")
     while True:
         print("Выберите № дальнейшего действия:\n"
               "1. Вывести топ 10 вакансий с самыми большими зарплатами.\n"
-              "2. Вывести первые 10 вакансий.\n"
-              "3. Вывести случайные 10 вакансий.\n"
+              "2. Вывести первые 20 вакансий.\n"
+              "3. Вывести случайные 15 вакансий.\n"
               "4. Выйти.")
         user = int(input(">>> "))
         if user == 1:
-            list = sorted(read_json_file(job), key=lambda v: v['salary'][0], reverse=True)
+            list = sorted(data, key=lambda v: v['salary'][0], reverse=True)
             res = list[:10]
             for r in res:
                 vacancy = Vacancy(r['title'], r['url'], r['salary'], r['description'])
                 print(vacancy)
         if user == 2:
-            res = read_json_file(job)[:10]
+            res = data[:20]
             for r in res:
                 print(Vacancy(r['title'], r['url'], r['salary'],
                               r['description']))
         if user == 3:
-            res = choices(read_json_file(job), k=10)
+            res = choices(data, k=15)
             for r in res:
                 print(Vacancy(r['title'], r['url'], r['salary'],
                               r['description']))
